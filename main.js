62cbe4a4e45f6a91596bd1d63d7bc8534b96db8b
booyaChael <chaeeul2022@gmail.com>
2022-08-18 11:23:25 +0900

    [update] toDoList 입력, 삭제(새로고침 시 초기화됨)

--- main.js
+++ main.js
@@ -50,33 +50,38 @@ document.getElementById("calendar_table_date").innerHTML = calendarTableDate;
 //h1에 현월 표시//
 document.getElementById("calendar_month").innerHTML = `${calendarMonth}월`;
 
-//showToDo//
+//toDoList 입력, 삭제//
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-let toDos = [];
-
+function deleteToDo(event){
+    console.log(event.target.parentElement);
+    const li = event.target.parentElement;
+    li.remove();
+}
 
 function handleToDoSubmit(event){ 
     event.preventDefault();
     const newToDo = toDoInput.value;
+    toDoInput.value = "";
     const li = document.createElement("li");
     const span = document.createElement("span");
     span.innerText = newToDo;
     const button = document.createElement("button");
     button.innerText = "X";
-    
 
     li.appendChild(span);
     li.appendChild(button);
 
     toDoList.appendChild(li);
+
+    button.addEventListener("click", deleteToDo);
 }
+
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-
+//날짜 띄우기
 function showToDo(element){
     alert(element.innerHTML);
-
 }
